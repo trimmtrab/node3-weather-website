c6311687ec7fa441ddde80329081413d913ce6b1
Artem Zimin <atm.zmn95@gmail.com>
2020-04-03 18:01:27 +0300

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,9 @@
-console.log("Client side javascript file is loaded!");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
-weatherForm.addEventListener("submit", event => {
+weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const location = search.value;
@@ -14,7 +12,7 @@ weatherForm.addEventListener("submit", event => {
   messageTwo.textContent = "";
 
   fetch(`/weather?address=${location}`)
-    .then(response => response.json())
+    .then((response) => response.json())
     .then(({ error, location, forecast }) => {
       if (error) {
         messageOne.textContent = error;
